cc97620829891bb99368959723fe2d5336e8cb8f
ordezachyo@mail.tau.ac.il <vd3n8thrw>
2021-06-30 15:05:15 +0300

    commit

--- main.py
+++ main.py
@@ -5,12 +5,6 @@ from Subject import *
 import matplotlib.pyplot as plt
 
 sub = Subject('AG1')
-a = sub.lab_sleep_score.replace([2,3,4,-1],1)
-a = pd.DataFrame.to_numpy(a)
-a = a.squeeze()
-
-
-fig,ax = plt.subplots()
-ax = a.plot()
+sub.plot_sleep_scores()
 
 print('test')
