b29902ff5b38b82020db5aa74891e1ed198903b8
ordezachyo@mail.tau.ac.il <vd3n8thrw>
2021-07-01 17:18:43 +0300

    Correlation script finish

--- Correlations.py
+++ Correlations.py
@@ -6,7 +6,10 @@ from yasa import sleep_statistics
 from main_with_inputs import load_subject
 import seaborn as sns
 
-def get_corr_analysis( param = ['SE','WASO','SME','TST','SPT']):
+
+
+
+def get_corr_data( param = ['SE','WASO','SME','TST','SPT']):
 
     Subjects, sub_list = load_subject()
     # Removing ME5 because it has only 1 previous night
@@ -15,29 +18,37 @@ def get_corr_analysis( param = ['SE','WASO','SME','TST','SPT']):
     night_list, night_lab, overlap = get_all_night_stats(Subjects, param)
     return  Subjects,sub_list,overlap,night_list,night_lab
 
-param = ['SE','WASO','SME','TST','SPT']
-Subjects, sub_list, overlap, night_list,night_lab = get_corr_analysis(param)
+def corr_plot():
 
-night_list.append(night_lab) # Add statistics from the lab before concat all data
+    param = ['SE','WASO','SME','TST','SPT']
+    Subjects, sub_list, overlap, night_list,night_lab = get_corr_data(param)
 
-df_all = pd.concat(night_list, axis=1)
-df_all - pd.concat([df_all,night_lab], axis = 1)
+    night_list.append(night_lab) # Add statistics from the lab before concat all data
 
-df_all.drop(['1st_SE','2nd_SE','3rd_SE','3rd_WASO','3rd_SME','3rd_TST','3rd_SPT'] ,axis = 1, inplace = True) # Drop all shit
+    df_all = pd.concat(night_list, axis=1)
+    df_all - pd.concat([df_all,night_lab], axis = 1)
 
-# Add means col
+    df_all.drop(['1st_SE','2nd_SE','3rd_SE','3rd_WASO','3rd_SME','3rd_TST','3rd_SPT'] ,axis = 1, inplace = True) # Drop all shit
 
-df_all['mean_SPT'] = df_all[['1st_SPT', '2nd_SPT']].mean(axis=1)
-df_all['mean_WASO'] = df_all[['1st_WASO', '2nd_WASO']].mean(axis=1)
-df_all['mean_TST'] = df_all[['1st_TST', '2nd_TST']].mean(axis=1)
-df_all['mean_SME'] = df_all[['1st_SME', '2nd_SME']].mean(axis=1)
+    # Add means col
 
-corr_matrix = df_all.astype(float).corr()
+    df_all['mean_SPT'] = df_all[['1st_SPT', '2nd_SPT']].mean(axis=1)
+    df_all['mean_WASO'] = df_all[['1st_WASO', '2nd_WASO']].mean(axis=1)
+    df_all['mean_TST'] = df_all[['1st_TST', '2nd_TST']].mean(axis=1)
+    df_all['mean_SME'] = df_all[['1st_SME', '2nd_SME']].mean(axis=1)
 
-fig, ax = plt.subplots(figsize=(12, 9))
-sns.set(font_scale=1)
-sns.heatmap(corr_matrix, annot=True,annot_kws = {"size": 10},cmap = 'coolwarm',square = True)
+    # Change some col names
+
+    df_all.rename( columns ={'SE' : 'SE_Lab', 'SME' :'SME_LAB','TST':'TST_Lab','SPT':'SPT_LAB','WASO':'WASO_Lab'}, inplace=True)
 
 
-sub = Subject('JR9',1,'22:48:00 02/18/2018')
-print('s')
+    corr_matrix = df_all.astype(float).corr()
+
+    fig, ax = plt.subplots(figsize=(12, 9))
+    fig.suptitle('Correlation Matrix')
+    sns.set(font_scale=1)
+    sns.heatmap(corr_matrix, annot=True,annot_kws = {"size": 10},cmap = 'coolwarm',square = True)
+
+
+
+
