e2665bb21dd98f7c6b4361c9b3b9df957bccce47
Gilad <giladsc@gmail.com>
2021-07-01 17:22:47 +0300

    Added correlations

--- main_with_inputs.py
+++ main_with_inputs.py
@@ -2,6 +2,7 @@ from Subject import *
 import datetime
 import sys
 import regression_analysis
+import correlation_matrix
 
 def load_subject():
     overlap_dict = {'AG1': [1, '23:48:00 07/25/2018'], 'CS7': [1, '21:29:00 10/23/2018'],
@@ -25,17 +26,18 @@ def load_subject():
 
 def instructions():
      # Setting the different messages for interacting with the user
-    print(f"Welcome to SleSco™ !\n\n")  # Welcome message
-    main_menu = (f"Choose the number of the action you would you like to perform:\n\
+    print("Welcome to SleSco™ !\n\n")  # Welcome message
+    main_menu = ("Choose the number of the action you would you like to perform:\n\
         1. Plot Sleep Data\n\
         2. Create Prediction Model - Previous Nights -> Sleep Lab\n\
-        3. Check Motionlogger to EEG Correlation\n\
+        3. Check Correlations - All Parameters\n\
         4. Examine Subjects List\n\n\
         At any time, you can go back to the main menu by typing 'main' or quit typing 'quit' \n")
     action_1 = ("To choose a specific subject, type his subject code\n\
         Alternativly, type 'all' to plot all subjects\n")
     action_2 = ("Choose the predicted variable:")
-    action_2_1 = (f"Choose predictors, seperate by pressing 'Enter'. Type 'end' to finish")
+    action_2_1 = ("Choose predictors, seperate by pressing 'Enter'. Type 'end' to finish")
+    action_3 = ("Creating correlation matrix")
     action_4 = ("If you would like to remove a subject, type his subject code\n\
         Reminder - you can go back to the main menu by typing 'main' or quit by typing 'quit' \n")
     reminder = ("Reminder - you can go back to the main menu by typing 'main' or quit by typing 'quit' ")
@@ -131,6 +133,10 @@ if __name__ == "__main__":
                         print ("Choose another predictor or type 'end' to run regression")
                         print (*var_list, sep=", ")
 
+        if main_choice == 3: #3. Check Correlations 
+            print(action_3)
+            correlation_matrix.get_corr_data()
+
         if main_choice == 4: #4. Examine Subjects List
             while True:
                 print ("Subjects list:")
@@ -149,7 +155,7 @@ if __name__ == "__main__":
                     sub_list.pop(is_sub)
                     Subjects.pop(is_sub)
                     continue
-        if main_choice>4:
+        if main_choice>4 or main_choice<=0: # input is integer but out of range
             print("Sorry, I didn't understand that.")
             #better try again... Return to the start of the loop
 
