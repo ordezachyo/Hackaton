33b69e667ae8368118dcb1678b1af2d032a81c11
NivSiton <smotH267>
2021-07-01 15:08:23 +0300

    finished indexing

--- Statistics.py
+++ Statistics.py
@@ -35,7 +35,7 @@ def get_all_night_stats(Subjects, param = ['SE','WASO','SME','TST','SPT']):
             overlap.at[i, par] = stats.get(par)
             overlap.at[i, 'Name'] = sub.name
     for night in night_list:
-        night.set_index('Name')
-    overlap.set_index('Name')
+        night.set_index('Name', inplace=True)
+    overlap.set_index('Name', inplace=True)
 
     return night_list, overlap
